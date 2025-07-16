1815f36aa4aa07a2ae07335d2d3c40c91a7ee109
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2025-07-16 23:40:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Server, createRouter } = require("sm-express-server");
 const routes = require("./routes.js");
 const bodyParser = require("body-parser");
 const express = require("express");
+const pool = require("./database.js");
 
 const port = process.env.PORT || 4005;
 const server = new Server(
@@ -12,6 +13,28 @@ const server = new Server(
     [routes]
 );
 
-server.start(() => {
+async function createTravelControlTable() {
+  const client = await pool.connect();
+  try {
+    const query = `
+      CREATE TABLE IF NOT EXISTS travelcontroll (
+        id SERIAL PRIMARY KEY,
+        total_money INTEGER,
+        money_spent INTEGER,
+        money_remaining INTEGER,
+        expenses JSONB
+      );
+    `;
+    await client.query(query);
+    console.log('Tabla "travelcontroll" lista.');
+  } catch (err) {
+    console.error('Error al crear/verificar tabla:', err);
+  } finally {
+    client.release();
+  }
+}
+
+server.start(async () => {
 	console.log(`Server is running on port ${port}`);
+	await createTravelControlTable();
 });
